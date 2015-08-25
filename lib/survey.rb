dd7c77656fb2a63b1cc6380609d84549698b1db2
George Denkey & Vaughn Dice <student@epicodus.com>
2015-08-25 09:57:35 -0700

    modify the survey spec as well as survey_rb, add done methods.

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,3 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
+  scope(:not_done, -> do
+  where({:done => false})
+end)
 end
--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -3,10 +3,20 @@ require('spec_helper')
 describe(Survey) do
   describe("#questions") do
     it("returns a list of questions within the survey") do
-    test_survey = Survey.create({:name => "Epicodus Survey", :done => false})
-    test_question1 = Question.create({:query => "Do you enjoy it here?", :survey_id => test_survey.id })
-    test_question2 = Question.create({:query => "What did you learn this week?", :survey_id => test_survey.id })
-    expect(test_survey.questions()).to(eq([test_question1, test_question2]))
+      test_survey = Survey.create({:name => "Epicodus Survey", :done => false})
+      test_question1 = Question.create({:query => "Do you enjoy it here?", :survey_id => test_survey.id })
+      test_question2 = Question.create({:query => "What did you learn this week?", :survey_id => test_survey.id })
+      expect(test_survey.questions()).to(eq([test_question1, test_question2]))
+    end
   end
- end
+
+  describe('.not_done') do
+    it('returns whether or not the survey has been completed') do
+      test_survey1 = Survey.create({:name => "Epicodus Survey", :done => false})
+      test_survey2 = Survey.create({:name => "Epicodus Survey 2", :done => false})
+      not_done_surveys = [test_survey1, test_survey2]
+      done_survey = Survey.create({:name => "All done", :done => true})
+      expect(Survey.not_done()).to(eq(not_done_surveys))
+    end
+  end
 end
