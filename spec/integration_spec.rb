6bb881882a466aba697a726181214f7e064c427f
George Denkey & Vaughn Dice <student@epicodus.com>
2015-08-25 15:23:51 -0700

    add back button to query page, pass spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,60 +5,70 @@ Capybara.app = Sinatra::Application
 
 describe("Surveyr", {:type => :feature}) do
 
- describe('the survey creation path') do
-   it("allows the survey designer to create a survey") do
-     visit('/')
-     fill_in("name", :with => 'epicodus Survey')
-     click_button('Submit')
-     expect(page).to have_content('Epicodus Survey')
-   end
-  end
+  describe('the survey creator stories') do
+   describe('the survey creation path') do
+     it("allows the survey designer to create a survey") do
+       visit('/')
+       fill_in("name", :with => 'epicodus Survey')
+       click_button('Submit')
+       expect(page).to have_content('Epicodus Survey')
+     end
+    end
 
-  it("allows the survey designer to create a question") do
-    survey = Survey.create({:name => 'My Survey', :done => false})
-    visit('/')
-    click_link(survey.name())
-    fill_in("query", :with => 'Do you enjoy programming?')
-    click_button('Create')
-    expect(page).to have_content('Do you enjoy programming?')
-  end
+    it("allows the survey designer to create a question") do
+      survey = Survey.create({:name => 'My Survey', :done => false})
+      visit('/')
+      click_link(survey.name())
+      fill_in("query", :with => 'Do you enjoy programming?')
+      click_button('Create')
+      expect(page).to have_content('Do you enjoy programming?')
+    end
 
-  it("allows the survey designer to update a survey with a new name") do
-    survey = Survey.create({:name => 'My Survey', :done => false})
-    visit("/surveys/#{survey.id()}")
-    fill_in("name", :with => 'New Survey Name')
-    click_button('UPDATE')
-    expect(page).to have_content('New Survey Name')
-  end
+    it("allows the survey designer to update a survey with a new name") do
+      survey = Survey.create({:name => 'My Survey', :done => false})
+      visit("/surveys/#{survey.id()}")
+      fill_in("name", :with => 'New Survey Name')
+      click_button('UPDATE')
+      expect(page).to have_content('New Survey Name')
+    end
 
-  it("allows the survey designer to delete a survey from the database") do
-    survey = Survey.create({:name => 'My Survey', :done => false})
-    visit("/surveys/#{survey.id()}")
-    click_button("DELETE")
-    expect(page).to have_content("Surveyr")
-    expect(page).to_not have_content('My Survey')
-  end
+    it("allows the survey designer to delete a survey from the database") do
+      survey = Survey.create({:name => 'My Survey', :done => false})
+      visit("/surveys/#{survey.id()}")
+      click_button("DELETE")
+      expect(page).to have_content("Surveyr")
+      expect(page).to_not have_content('My Survey')
+    end
 
-  it("allows the survey designer to remove questions from a survey") do
-   survey = Survey.create({:name => 'My Survey', :done => false})
-   question = Question.create({:query => "Do you enjoy programming?", :survey_id => survey.id})
-   visit("/surveys/#{survey.id()}")
-   click_link(question.query())
-   click_button("Delete Query")
-   expect(page).to have_content("My Survey")
-   expect(page).to_not have_content("Do you enjoy programming?" )
-  end
+    it("allows the survey designer to remove questions from a survey") do
+     survey = Survey.create({:name => 'My Survey', :done => false})
+     question = Question.create({:query => "Do you enjoy programming?", :survey_id => survey.id})
+     visit("/surveys/#{survey.id()}")
+     click_link(question.query())
+     click_button("Delete Query")
+     expect(page).to have_content("My Survey")
+     expect(page).to_not have_content("Do you enjoy programming?" )
+    end
 
-  it("allows the survey designer to add possible answers to a question") do
-    survey = Survey.create({:name => 'My Survey', :done => false})
-    question = Question.create({:query => "Do you enjoy programming?", :survey_id => survey.id})
-    visit("/queries/#{question.id()}")
-    fill_in("answer", :with => 'yes')
-    click_button("Add Answer")
-    fill_in("answer", :with => 'no')
-    click_button("Add Answer")
-    expect(page).to have_content("Yes")
-    expect(page).to have_content("No")
+    it("allows the survey designer to add possible answers to a question") do
+      survey = Survey.create({:name => 'My Survey', :done => false})
+      question = Question.create({:query => "Do you enjoy programming?", :survey_id => survey.id})
+      visit("/queries/#{question.id()}")
+      fill_in("answer", :with => 'yes')
+      click_button("Add Answer")
+      fill_in("answer", :with => 'no')
+      click_button("Add Answer")
+      expect(page).to have_content("Yes")
+      expect(page).to have_content("No")
+    end
+
+    it('allows the survey designer to return to the unique survey list from a question page') do
+      survey = Survey.create({:name => 'My Survey', :done => false})
+      question = Question.create({:query => "Do you enjoy programming?", :survey_id => survey.id})
+      visit("/queries/#{question.id()}")
+      click_link('Back')
+      expect(page).to have_content('My Survey')
+    end
   end
 
   describe("Surveytaker") do
@@ -81,6 +91,6 @@ describe("Surveyr", {:type => :feature}) do
       visit('/')
       click_link("Take Survey")
       expect(page).to have_content('Here are your surveys')
+    end
   end
- end
 end
